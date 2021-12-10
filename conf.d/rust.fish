b9c5060795be925bad6ad819b509bfa4219ba6a7
Wolfgang Senfter <w.senfter@gmail.com>
2021-12-10 08:12:58 +0100

    integrate cargo install

--- conf.d/rust.fish
+++ conf.d/rust.fish
@@ -5,8 +5,16 @@ function _rust_install
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 end
 
+function _cargon_install_update
+    cargo install cargo-update
+end
+
 function _rust_completions
     rustup completions fish >"$HOME/.config/fish/completions/rust.fish"
+end
+
+function _cargo_update_crates
+    cargo install-update -a
 end
 
 function _on_rust_install --on-event rust_install
@@ -15,10 +23,12 @@ function _on_rust_install --on-event rust_install
     end
 
     _rust_completions
+    _cargon_install_update
 end
 
 function _on_postexec_rust_update --on-event fish_postexec
     if string match -qr '^rustup update$' $argv
         _rust_completions
+        _cargo_update_crates
     end
 end
